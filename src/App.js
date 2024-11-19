8b462aad422740a0faa5d279df22d7127bca5439
Miljan <miljan.milojkovic22@gmail.com>
2024-11-19 23:12:51 +0100

    fixed overflow

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ScrollToTop from './components/Other Components/ScrollToTop';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App overflow-x-hidden">
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
